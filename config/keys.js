95f437b1f596ac371869d770c3f6265c908ca69d
Grigore <58747106+Grigore94@users.noreply.github.com>
2021-02-21 18:22:50 -0600

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admingreg:admingreg@cluster0.lj9yi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://<Yourname>:<Yourpassword>@cluster0.lj9yi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
